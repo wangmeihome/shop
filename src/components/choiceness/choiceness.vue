b6ce47c8abbb36bae1f944deb10ec88a0c4862b6
mofi <mofigg@163.com>
2017-10-30 18:54:06 +0800

    index finish

---
+++ src/components/choiceness/choiceness.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="choice_wrapper">
+    <div class="choice">
+      <h1 class="choice_title">{{usingImg[0].des}}</h1>
+    </div>
+    <div class="choice_list" style="overflow: hidden">
+      <div class="imgBanner">
+        <img :src="usingImg[0].src">
+      </div>
+      <div v-for="item in 6" class="choice_list_item">
+        <a href="#">
+          <img src="../../assets/products/product.jpg">
+          <h3>风向仪</h3>
+          <p>￥ 2799</p>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    usingImg:{
+      type:Array,
+      default:[]
+    }
+  }
+}
+</script>
+
+<style>
+  .choice_wrapper{
+    width: 1218px;
+    margin: 0 auto;
+    background: white;
+    border: 1px solid #dcdcdc;
+    border-radius: 8px;
+    overflow: hidden;
+    margin-top: 30px;
+  }
+  .choice{
+    width: 1218px;
+    height: 60px;
+    border-bottom: 1px solid #d4d4d4;
+    border-radius:8px 8px 0 0;
+    line-height: 60px;
+    background:linear-gradient(#fbfbfb, #ececec);
+    padding-left: 30px;
+  }
+  .imgBanner{
+    float: left;
+    width: 50%;
+    height: 430px;
+  }
+  .imgBanner img{
+    width: 609px;
+    height: 430px;
+    display: block;
+  }
+  .choice_list_item{
+    width: 25%;
+    height: 430px;
+    float: left;
+  }
+  .choice_list_item img{
+    width: 206px;
+    height: 206px;
+    display: block;
+    margin: 50px auto 10px;
+  }
+  .choice_list_item h3{
+    line-height: 1.2;
+    font-size: 16px;
+    color: #424242;
+    margin: 0 auto;
+    padding: 20px 14px;
+    text-align: center;
+    overflow: hidden;
+  }
+  .choice_list_item p{
+    margin: 0 auto;
+    text-align: center;
+    color: red;
+    margin-top: 20px;
+  }
+</style>
--- src/pages/Home/home.vue
+++ src/pages/Home/home.vue
@@ -8,13 +8,57 @@
       <div class="banner">
         <div class="bg" ref="bg" @mouseover="bgOver($refs.bg)" @mousemove="bgMove($refs.bg,$event)" @mouseout="bgOut($refs.bg)"></div>
       </div>
+      <div class="hot_wrapper">
+        <div class="hot">
+          <h1 class="hot_title">热卖商品</h1>
+        </div>
+        <div class="hot_list" style="overflow: hidden">
+
+          <!-- 替代无数据 -->
+          <div v-for="item in 3" class="hot_list_item">
+            <a href="#">
+              <img src="../../assets/products/product.jpg">
+              <h3>风向仪</h3>
+              <p>￥ 2799</p>
+            </a>
+          </div>
+
+        </div>
+      </div>
+      <choiceness :usingImg="choicenessImg"></choiceness>
+      <choiceness :usingImg="brandImg"></choiceness>
+      <div class="partner">
+        <h1>合作伙伴</h1>
+        <div><img src="../../assets/partnerImgs/atlas.jpg"></div>
+        <div><img src="../../assets/partnerImgs/bussmann.jpg"></div>
+        <div><img src="../../assets/partnerImgs/fag.jpg"></div>
+        <div><img src="../../assets/partnerImgs/ft.jpg"></div>
+        <div><img src="../../assets/partnerImgs/shine.jpg"></div>
+        <div><img src="../../assets/partnerImgs/skf.jpg"></div>
+      </div>
     </div>
 </template>
 
 <script>
+  import choiceness from '../../components/choiceness/choiceness.vue'
   export default {
+    components:{
+      choiceness
+    },
     data () {
       return {
+        choicenessImg:[
+          {
+            src:require('../../assets/others/choiceness.jpg'),
+            des:"官方精选"
+          }
+        ],
+        brandImg:[
+          {
+            src:require('../../assets/others/choiceness.jpg'),
+            des:"品牌精选"
+          }
+        ],
         queryInfo:'',
         bgOpt: {
           px: 0,
@@ -65,8 +109,9 @@
   .home{
     padding-top: 50px;
     width: 100%;
-    height: 1000px;
-    background: ghostwhite;
+    /*height: 1000px;*/
+    padding-bottom: 100px;
+    /*background: green;*/
   }
   .inquire{
     width: 700px;
@@ -96,4 +141,95 @@
     -webkit-transform-origin: 50% 50%;
     -webkit-transform: rotateY(0deg) rotateX(0deg);
   }
+  .hot_wrapper{
+    width: 1218px;
+    margin: 0 auto;
+    background: white;
+    border: 1px solid #dcdcdc;
+    border-radius: 8px;
+    overflow: hidden;
+  }
+  .hot{
+    width: 1218px;
+    height: 60px;
+    border-bottom: 1px solid #d4d4d4;
+    border-radius:8px 8px 0 0;
+    line-height: 60px;
+    background:linear-gradient(#fbfbfb, #ececec);
+    padding-left: 30px;
+  }
+  .hot_list_item{
+    float: left;
+    width: 406px;
+    height: 430px;
+  }
+  .hot_list_item img{
+    width: 206px;
+    height: 206px;
+    display: block;
+    margin: 50px auto 10px;
+  }
+  .hot_list_item h3{
+    line-height: 1.2;
+    font-size: 16px;
+    color: #424242;
+    margin: 0 auto;
+    padding: 20px 14px;
+    text-align: center;
+    overflow: hidden;
+  }
+  .hot_list_item p{
+    margin: 0 auto;
+    text-align: center;
+    color: red;
+    margin-top: 20px;
+  }
+  .partner{
+    width: 1220px;
+    height: 100px;
+    margin: 0 auto;
+    margin-top: 50px;
+    border-top: 1px solid #d4d4d4;
+    position: relative;
+  }
+  .partner h1{
+    position: absolute;
+    top: -25px;
+    left: 0;
+  }
+  .partner div{
+    float: left;
+    width: 180px;
+    height: 80px;
+    margin-top: 10px;
+    padding-right: 22.5px;
+    border-right: 1px solid #ccc;
+  }
+  .partner div:last-child{
+    border-right: none;
+  }
+  .partner div img{
+    width: 100%;
+    height: 100%;
+  }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
