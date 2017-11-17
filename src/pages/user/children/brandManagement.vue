3448d6467316a5b48426cf1fb3820b411b25ebec
mofi <mofigg@163.com>
2017-11-17 16:24:22 +0800

    brand

--- src/pages/user/children/brandManagement.vue
+++ src/pages/user/children/brandManagement.vue
@@ -4,12 +4,13 @@
         <h1>品牌管理</h1>
       </div>
       <div class="brand_nav">
+
         <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-          <el-tab-pane label="品牌列表" name="first">
+          <el-tab-pane label="品牌列表" name="brandList">
             <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
               <el-table-column type="selection" width="55"></el-table-column>
-              <el-table-column prop="num" label="序号" width="120"></el-table-column>
-              <el-table-column prop="brandName" label="品牌名称" width="240"></el-table-column>
+              <el-table-column prop="num" label="序号" width="55"></el-table-column>
+              <el-table-column prop="brandName" label="品牌名称" width="120"></el-table-column>
               <el-table-column label="品牌LOGO" width="120">
                 <template slot-scope="scope">
                   <img :src="scope.row.brandImg" style="width: 120px;height: 50px">
@@ -38,9 +39,17 @@
                   </div>
                 </template>
               </el-table-column>
+              <el-table-column prop="info" label="状态" width="120"
+                               :filters="[{ text: '审核未通过', value: '审核未通过' }, { text: '已审核', value: '已审核' },{ text: '审核中', value: '审核中' }]"
+                               :filter-method="filterTag"
+                               filter-placement="bottom-end">
+                <template slot-scope="scope">
+                  <el-tag :type="scope.row.info === '审核未通过' ? 'danger' : scope.row.info === '已审核' ? 'success' : 'warning'" close-transition>{{scope.row.info}}</el-tag>
+                </template>
+              </el-table-column>
             </el-table>
           </el-tab-pane>
-          <el-tab-pane label="新增品牌" name="second">新增品牌</el-tab-pane>
+          <el-tab-pane label="新增品牌" name="addBrand">新增品牌</el-tab-pane>
         </el-tabs>
       </div>
     </div>
@@ -50,39 +59,46 @@
 export default {
   data(){
     return {
-      activeName: 'first',
-      tableData: [{
+      activeName: 'brandList',
+      tableData: [
+        {
         num: 1,
         brandName: '品牌1',
         brandImg: require('../../../assets/partnerImgs/atlas.jpg'),
-        auditStatus:0
+        auditStatus:0,
+        info:'审核未通过'
       }, {
         num: 2,
         brandName: '品牌2',
         brandImg: require('../../../assets/partnerImgs/atlas.jpg'),
-        auditStatus:1
+        auditStatus:1,
+        info:'已审核'
       }, {
         num: 3,
         brandName: '品牌3',
         brandImg: require('../../../assets/partnerImgs/atlas.jpg'),
-        auditStatus:2
+        auditStatus:2,
+        info:'审核中'
       }, {
         num: 4,
         brandName: '品牌4',
         brandImg: require('../../../assets/partnerImgs/atlas.jpg'),
-        auditStatus:2
+        auditStatus:2,
+        info:'审核中'
       }, {
         num: 5,
         brandName: '品牌5',
         brandImg: require('../../../assets/partnerImgs/atlas.jpg'),
-        auditStatus:1
+        auditStatus:1,
+        info:'已审核'
       }, {
         num: 6,
         brandName: '品牌6',
         brandImg: require('../../../assets/partnerImgs/atlas.jpg'),
-        auditStatus:0
+        auditStatus:0,
+        info:'审核未通过'
       }],
-      multipleSelection: []
+      multipleSelection: [],
     }
   },
   methods:{
@@ -91,6 +107,7 @@ export default {
     },
     handleSelectionChange(val) {
       this.multipleSelection = val;
+      console.log(val)
     },
 //    查看操作
     examine(index, row){
@@ -107,6 +124,13 @@ export default {
 //    取消重新申请
     cancelApply(index,row){
       console.log(index, row);
+    },
+    filterTag(value, row) {
+      return row.info === value;
+    },
+//    选择品牌审核状态
+    getStatus(){
+      console.log(this.selectValue)
     }
   }
 }
@@ -131,7 +155,7 @@ export default {
     background:linear-gradient(#fbfbfb, #ececec);
     font-family: "Microsoft YaHei UI";
   }
-  .brand_nav{
+  .brand_nav .el-table{
     text-align: center;
   }
   .brand_nav .has-gutter tr th{
