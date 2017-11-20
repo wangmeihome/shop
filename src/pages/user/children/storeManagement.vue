19127f97e42105800ccde9c858c875076df4f4f7
mofi <mofigg@163.com>
2017-11-20 12:04:07 +0800

    edit store

--- src/pages/user/children/storeManagement.vue
+++ src/pages/user/children/storeManagement.vue
@@ -14,19 +14,11 @@
               </div>
               <div class="store_wrapper_item">
                 <p class="banner">店铺banner：</p>
-                <el-upload multiple class="upload-demo" list-type="picture" ref="bannerUpload" action="" :on-success="bannerSuccess" :on-change="bannerOnChange" :on-preview="bannerHandlePreview" :on-remove="bannerHandleRemove" :auto-upload="false">
-                  <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                  <el-button style="margin-left: 10px;" size="small" type="success" @click="bannerSubmitUpload" :disabled="bannerBtn">上传到服务器</el-button>
-                  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过2MB</div>
-                </el-upload>
+                <storePic :theAction="storeAction" @getOnePic="getBannerUrl"></storePic>
               </div>
               <div class="store_wrapper_item">
                 <p class="logo">店铺logo：</p>
-                <el-upload class="upload-demo" list-type="picture" ref="logoUpload" action="" :on-success="logoSuccess" :on-change="logoOnChange" :on-preview="logoHandlePreview" :on-remove="logoHandleRemove" :auto-upload="false">
-                  <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                  <el-button style="margin-left: 10px;" size="small" type="success" @click="logoSubmitUpload" :disabled="logoBtn">上传到服务器</el-button>
-                  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过2MB</div>
-                </el-upload>
+                <storePic :theAction="storeAction" @getOnePic="getLogoUrl"></storePic>
               </div>
               <div class="store_wrapper_item">
                 <p class="storeInfo">店铺简介：</p>
@@ -38,7 +30,6 @@
                   <quill-editor @blur="onEditorBlur($event)" v-model="content"></quill-editor>
                 </div>
               </div>
-
               <div style="text-align: center;margin-top: 120px">
                 <div>
                   <el-button @click="submitStoreInfo" type="primary">提交</el-button>
@@ -53,110 +44,42 @@
 </template>
 
 <script>
+  import storePic from '../../../components/upload/uploadOnePic.vue'
   export default {
+    components:{
+      storePic
+    },
     data() {
       return {
-        activeName: 'storeDetails',
+        storeAction:'/store/upload.ajax',
+        activeName: 'storeSet',
         storeName:'',
-//    公司营业执照的路径
-        logoUrlArr:[],
-//    判断上传营业执照图片大小的文字提示框
-        logoDialogVisible: false,
-//    判断上传营业执照大小的按钮
-        logoBtn: true,
-//    其他附件的路径
-        bannerUrlArr:[],
-//    判断上传其他附件图片大小的文字提示框
-        bannerDialogVisible: false,
-//    判断上传其他附件大小的按钮
-        bannerBtn: true,
         aboutStore:'',
-        content:''
+        content:'',
+        bannerUrl:'',
+        logoUrl:''
       };
     },
     methods: {
       handleClick(tab, event) {
-        console.log(tab, event);
-      },
-//    当选取的文件大小超过2M的时候的钩子函数
-      bannerOnChange(file, fileList) {
-        if(file.size >= 2097152) {
-          this.bannerDialogVisible = true;
-          this.bannerBtn = true;
-          fileList.pop();
-          if(fileList.length === 0){
-            this.bannerBtn = true;
-          }else{
-            this.bannerBtn = false;
-          }
-        } else {
-          this.bannerBtn = false;
-        }
-      },
-//    上传banner
-      bannerSubmitUpload() {
-        this.$refs.bannerUpload.submit();
-      },
-      bannerSuccess(response, file, fileList) {
-        this.otherUrlArr.push(response.data);
-        console.log(this.otherUrlArr);
-      },
-//    移除列表文件时的钩子函数
-      bannerHandleRemove(file, fileList) {
-//      console.log(file, fileList);
-        this.bannerBtn = true;
-      },
-//    点击已上传文件的钩子函数
-      bannerHandlePreview(file) {
-
-      },
-//    点击banner大于2M的文字提示框的关闭按钮的钩子函数
-      logoHandleClose() {
-        this.licenseDialogVisible = false;
-      },
-//    点击logo大于2M的文字提示框的关闭按钮的钩子函数
-      bannerHandleClose() {
-        this.bannerDialogVisible = false;
-      },
-//    当选取的文件大小超过2M的时候的钩子函数
-      logoOnChange(file, fileList) {
-        if(file.size >= 2097152) {
-          this.licenseDialogVisible = true;
-          this.licenseBtn = true;
-          fileList.pop();
-          if(fileList.length === 0){
-            this.licenseBtn = true;
-          }else{
-            this.licenseBtn = false;
-          }
-        } else {
-          this.licenseBtn = false;
-        }
-      },
-//    上传logo
-      logoSubmitUpload() {
-        this.$refs.logoUpload.submit();
-      },
-//    上传营业执照成功后的钩子函数
-      logoSuccess(response, file, fileList) {
-        this.logoUrlArr.push(response.data);
-        console.log(this.logoUrlArr);
-      },
-//    移除列表文件时的钩子函数
-      logoHandleRemove(file, fileList) {
-//      console.log(file, fileList);
-        this.logoBtn = true;
-      },
-//    点击已上传文件的钩子函数
-      logoHandlePreview(file) {
-//      目前无逻辑
+//        console.log(tab, event);
       },
 //      填写完店铺详情后失去焦点的钩子函数
       onEditorBlur(editor) {
         console.log(this.content);
       },
+      getBannerUrl(theUrl){
+        this.bannerUrl = theUrl;
+      },
+      getLogoUrl(theUrl){
+        this.logoUrlArr = theUrl;
+      },
       submitStoreInfo(){
-        alert("提交店铺信息");
+        console.log(this.storeName);
+        console.log(this.bannerUrl);
+        console.log(this.logoUrl);
+        console.log(this.aboutStore);
+        console.log(this.content);
       }
     }
   };
